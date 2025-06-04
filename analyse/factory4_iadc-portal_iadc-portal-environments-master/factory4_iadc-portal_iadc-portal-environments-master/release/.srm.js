6d96b3421a9925a087e9d3a7d9d2f40dc2a08164
Aznar <Jaime.Aznar@sanofi.com>
2025-06-04 17:36:10 +0200

    adding files

---
+++ analyse/factory4_iadc-portal_iadc-portal-environments-master/factory4_iadc-portal_iadc-portal-environments-master/release/.srm.js
@@ -0,0 +1,5 @@
+module.exports = {
+  // URL of the GitLab repository
+  gitlabRepositoryUrl:
+    'https://emea-aws-gitlab.sanofi.com:3001/factory4/digital-foundation/iadc-portal/iadc-portal-environments'
+}
